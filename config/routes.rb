df18bd4ad5adb50ac0c7aa7c9ca3c20a6085b68e
johnmary1 <johnmary1@gmail.com>
2023-10-26 20:36:41 -0400

    Added update action

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,7 @@ Rails.application.routes.draw do
 
   get "/products/:id" => "products#show"
 
+  post "/products" => "products#create"
+
+  patch "/products/:id" => "products#update"
 end
--- test/controllers/products_controller_test.rb
+++ test/controllers/products_controller_test.rb
@@ -16,4 +16,19 @@ class ProductsControllerTest < ActionDispatch::IntegrationTest
     data = JSON.parse(response.body)
     assert_equal ["id", "name", "price", "description"], data.keys
   end
+
+  test "create" do
+    assert_difference "Product.count", 1 do
+      post "/products.json", params: { name: "Book 234", price: 85, image_url: "http://test.com/test.jpg", description: "N/A N/A N/A" }
+    end
+  end
+
+  test "update" do
+    product = Product.first
+    patch "/products/#{product.id}.json", params: { name: "Update.This.Name." }
+    assert_response 200
+
+    data = JSON.parse(response.body)
+    assert_equal "Update.This.Name", data["name"]
+  end
 end
